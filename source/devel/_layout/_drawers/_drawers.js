aaad627a9bae6c5bd12d3093d36e304349001a0a
Ragnarok <DeathMetallDrummer@gmail.com>
2019-01-23 18:06:54 +0600

    Drawers

---
+++ source/devel/_layout/_drawers/_drawers.js
@@ -0,0 +1,6 @@
+$('.drawer__toggle_left').on('click',function() {
+	$('.drawers').toggleClass('drawer__opened_left');
+})
+$('.drawer__toggle_right').on('click',function() {
+	$('.drawers').toggleClass('drawer__opened_right');
+})
